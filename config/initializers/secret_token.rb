b758c5b2cabf72659a3f9089c7d313d4ab5c6f9e
vince carey <vince@vincemcarey.com>
2011-08-08 21:07:21 -0400

    my2ndapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd6cb6b75503481b24a14c93734e425890a98f09dd848c5f6d393f687b4ce7ccae5cd3faa9ccc0fa33bdef299b09f8615aa9df750718692c38c047c4621726771'
